2e37b4c772e6697814980d0a7735ca47838872e2
NikosZoros3533 <nikozoro@csd.auth.gr>
2025-09-19 01:37:14 +0300

    First setup and Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { Home, Sprout } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 w-full bg-primary backdrop-blur supports-[backdrop-filter]:bg-primary/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16 ">
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-lg font-bold text-primary-background tracking-wider"
+            >
+              🍸REVELRY
+            </Link>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/places">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Places</span>
+              </Link>
+            </Button>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <Home className="w-4 h-4" />
+                <span className="hidden lg:inline">Home</span>
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
