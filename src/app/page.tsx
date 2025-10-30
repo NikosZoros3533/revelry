3d8ba08af272a68e2fc4a94bfbd6a17a413a9bf6
NikosZoros3533 <nikos7kozani@gmail.com>
2025-10-30 15:54:55 +0200

    hero page v.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,67 @@
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+import CircularGallery from "./../components/CircularGallery";
+
 export default function Home() {
+  const items = [
+    { image: "/Alex.jpg", text: "Alexandroupoli" },
+    { image: "/Athens.jpg", text: "Athens" },
+    { image: "/Thess.jpg", text: "Thessaloniki" },
+    { image: "/Giannena.jpg", text: "Ioannena" },
+    { image: "/Hrakleio.jpg", text: "Hrakleio" },
+    { image: "/Volos.jpg", text: "Volos" },
+  ];
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col  items-center sm:items-start">
-        Hello Worlds
-      </main>
-    </div>
+    <>
+      <div className="max-h-screen w-full flex flex-col items-center justify-center px-6 py-10 gap-14 mt-20">
+        <div className="text-center max-w-3xl">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl md:leading-[1.2] font-semibold tracking-tighter">
+            Make your vote for the{" "}
+            <span className="text-primary tracking-wide">Revelry</span> of your
+            Town
+          </h1>
+          <p className="mt-6 md:text-lg text-secondary/70">
+            You can have 4 votes per month.Every city has a 5 places for
+            shoutout the rotation changes each month.
+          </p>
+          <p className=" md:text-lg text-secondary/70">
+            Your vote makes places make the cut.
+          </p>
+          <div className="mt-12 flex items-center justify-center gap-4">
+            <Button size="lg" className="rounded-full text-lg">
+              Play your votes
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              className="rounded-full text-base shadow-none hover:text-primary"
+            >
+              City Ladders
+            </Button>
+          </div>
+        </div>
+        {/* <div className="flex gap-4 px-6 py-10 max-w-full">
+        {items.map((image) => (
+          <Image
+            className="rounded-2xl"
+            key={image.src}
+            src={image.src}
+            alt="dontknow"
+            width={500}
+            height={500}
+          />
+        ))}
+      </div> */}
+      </div>
+      <div style={{ height: "500px", position: "relative" }}>
+        <CircularGallery
+          bend={3}
+          textColor="#f2f5fc"
+          borderRadius={0.05}
+          scrollEase={0.02}
+          items={items}
+        />
+      </div>
+    </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,18 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Home, LogIn, Martini } from "lucide-react";
 import { stackServerApp } from "@/stack/server";
-import { getUserDetails } from "@/lib/actions";
+// import { getUserDetails } from "@/lib/actions";
 import { UserButton } from "@stackframe/stack";
 
 const Navbar = async () => {
   const user = await stackServerApp.getUser();
   const app = stackServerApp.urls;
-  const userProfile = await getUserDetails(user?.id);
+  //  const userProfile = await getUserDetails(user?.id);
 
-  console.log(userProfile?.name);
+  // console.log(userProfile?.name);
 
   return (
-    <nav className="sticky top-6 inset-x-4 h-16 bg-primary mb-8 backdrop-blur supports-[backdrop-filter]:bg-primary/60 z-50  max-w-(--breakpoint-xl) mx-auto rounded-full">
+    <nav className="sticky top-6 h-16 bg-primary mb-8 backdrop-blur supports-[backdrop-filter]:bg-primary/60 z-50  max-w-(--breakpoint-xl) mx-auto rounded-full">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16 ">
           <div className="flex items-center">
