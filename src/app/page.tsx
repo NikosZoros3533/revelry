ab5d0cfd88a3f7365106ae8eba5961bbbd495ac7
NikosZoros3533 <nikozoro@csd.auth.gr>
2025-12-07 21:22:08 +0200

    update greeting message to display 'none' for undefined user names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col  items-center sm:items-start">
         Hello Worlds
-        <div>{user ? `Hello, ${user.displayName ?? "anon"}` : 'You are not logged in'}</div>
+        <div>{user ? `Hello, ${user.displayName ?? "none"}` : 'You are not logged in'}</div>
       </main>
     </div>
   );
